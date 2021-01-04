ec5be608ad0eea6d65534f90dca8380a969798db
Ilya Cherkasov <ilya.r.cherkasov@gmail.com>
2021-01-04 18:05:04 +0300

    UICollectionView table part 1

---
+++ Avito Test/CustomCollectionViewCell.swift
@@ -0,0 +1,69 @@
+//
+//  CollectionViewCell.swift
+//  Avito Test
+//
+//  Created by Ilya Cherkasov on 04.01.2021.
+//
+
+import UIKit
+
+class CustomCollectionViewCell: UICollectionViewCell {
+    
+    static let indentifier = "CustomCollectionView"
+    
+    private let iconImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "vas-xl-52")
+        //imageView.backgroundColor = .yellow
+        return imageView
+    }()
+    
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "XL-объявление"
+        return label
+    }()
+    
+    private let descriptionLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Пользователи смогут посмотреть фотографии, описание и телефон прямо из результатов поиска. Пользователи смогут посмотреть фотографии, описание и телефон прямо из результатов поиска"
+        //label.backgroundColor = .yellow
+        return label
+    }()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        contentView.addSubview(titleLabel)
+        contentView.addSubview(descriptionLabel)
+        contentView.addSubview(iconImageView)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        self.layer.backgroundColor = CGColor(red: 0.5, green: 0.5, blue: 0.5, alpha: 0.2)
+        self.layer.cornerRadius = 10.0
+        
+        iconImageView.frame = CGRect(x: 15,
+                               y: 15,
+                               width: 55,
+                               height: 55)
+        
+        titleLabel.frame = CGRect(x: 100,
+                               y: 15,
+                               width: contentView.frame.size.width - 115,
+                               height: 50)
+        titleLabel.font = UIFont.boldSystemFont(ofSize: 24.0)
+        
+        descriptionLabel.frame = CGRect(x: 100,
+                               y: 50,
+                               width: contentView.frame.size.width - 115,
+                               height: 100)
+        descriptionLabel.lineBreakMode = .byWordWrapping
+        descriptionLabel.numberOfLines = 0
+        descriptionLabel.sizeToFit()
+    }
+}
--- Avito Test/ViewController.swift
+++ Avito Test/ViewController.swift
@@ -7,14 +7,48 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    
+    private var collectionView: UICollectionView?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = .blue
-        // Do any additional setup after loading the view.
+        self.view.backgroundColor = .white
+        //ClearButton
+        //TextLabel
+        
+        // CollectionView
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        layout.itemSize = CGSize(width: view.bounds.width - 40, height: view.frame.size.height / 5)
+        
+        let frame = CGRect(x: 20, y: 200, width: view.bounds.width - 40, height: view.bounds.height - 400)
+        
+        collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        guard let collectionView = collectionView else {
+            return
+        }
+        collectionView.register(CustomCollectionViewCell.self, forCellWithReuseIdentifier: CustomCollectionViewCell.indentifier)
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        collectionView.frame = frame
+        collectionView.backgroundColor = .white
+        view.addSubview(collectionView)
     }
 
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 30
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CustomCollectionViewCell.indentifier, for: indexPath)
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print("\(indexPath)")
+    }
 
 }
 
